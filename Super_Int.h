03ff9dec484bd898e048b43d87c9f8f29a31dcde
Volzeno <Mr.messaoudene1@gmail.com>
2020-01-11 12:14:42 +0100

    removed all the const param

--- Super_Int.h
+++ Super_Int.h
@@ -10,10 +10,10 @@ public:
     Super_int(int chifre);
     ~Super_int();
     /*******Operator_Methods******/
-    Super_int&  operator=  (Super_int const& b);
+    Super_int&  operator=  (Super_int & b);
     Super_int&  operator=  (int chifre);
     Super_int&  operator+= (int chifre);
-    Super_int&  operator+= (Super_int const& a);
+    Super_int&  operator+= (Super_int & a);
     Super_int&  operator-= (int chifre);
     Super_int&  operator-= (Super_int & a);
     /***********Methods**********/
@@ -24,17 +24,18 @@ public:
 private:
     void Set_Value(int i,int c);
     void Push_Value(int c);
+    void Set_Signe(int s);
     std::vector<int> Number; // Note to self Remember that the digits are stored from Less Significant Digit (LSD) to Most Significant Digit (MSD)
-    bool signe;// false -> negative
+    int signe=1;// -1 -> negative / 1 -> positive
 };
 
 //Super_int  operator% (Super_int &a,Super_int &b);
-Super_int  operator* (Super_int const& a,Super_int const& b);
+Super_int  operator* (Super_int & a,Super_int & b);
 Super_int  operator* (Super_int & a,int chifre);
 //Super_int  operator/ (Super_int &a,Super_int &b);
-Super_int  operator- (Super_int const&a,Super_int &b);
+Super_int  operator- (Super_int &a,Super_int &b);
 Super_int  operator- (Super_int &a,int chifre);
-Super_int  operator+ (Super_int const& a,Super_int & b);
+Super_int  operator+ (Super_int & a,Super_int & b);
 Super_int  operator+ (Super_int & a,int chifre);
 
 ////////////////// Comparative operators  ///////////////////////
